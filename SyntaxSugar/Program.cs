cf0f57eee541fef1db8add79f45c8cd300ce243b
Zachary Whitley <zbwhitley@gmail.com>
2022-02-15 13:28:01 -0600

    Add project files.

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Given the following code, change the code to implement inferred typing, string interpolation, and the ternary operator
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+            
+            //// Inferred, String Interpolation, and Ternary
+            Console.WriteLine("Please select a number 1-10");
+            var answer = int.Parse(Console.ReadLine());
+
+            var response = ((answer) < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
